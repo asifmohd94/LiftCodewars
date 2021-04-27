67d2260b22fe15aa8464273c1600af7ac281036d
asifmohd94 <asf.mohd@gmail.com>
2021-04-27 12:50:54 +0530

    calculator api using typescript

--- calculator-api-ts/build/routes/calc.js
+++ calculator-api-ts/build/routes/calc.js
@@ -10,13 +10,15 @@ const history = [];
 exports.router.post('/add', (req, res) => {
     const operation = {
         operands: req.body.operand,
-        result: 0
+        result: 0,
+        operationPerformed: ""
     };
     let sum = 0;
     for (let i of operation.operands) {
         sum += i;
     }
     operation.result = sum;
+    operation.operationPerformed = "+";
     history.push(operation);
     res.send(operation);
 });
@@ -24,52 +26,60 @@ exports.router.post('/subtract', (req, res) => {
     console.log("hello I m running");
     const operation = {
         operands: req.body.operand,
-        result: 0
+        result: 0,
+        operationPerformed: ""
     };
     let minus = operation.operands[0];
     for (let i = 1; i < operation.operands.length; i++) {
         minus -= operation.operands[i];
     }
     operation.result = minus;
+    operation.operationPerformed = "-";
     history.push(operation);
     res.send(operation);
 });
 exports.router.post('/multiply', (req, res) => {
     const operation = {
         operands: req.body.operand,
-        result: 0
+        result: 0,
+        operationPerformed: ""
     };
     let product = 1;
     for (let i of operation.operands) {
         product *= i;
     }
     operation.result = product;
+    operation.operationPerformed = "*";
     history.push(operation);
     res.send(operation);
 });
 exports.router.post('/divide', (req, res) => {
     const operation = {
         operands: req.body.operand,
-        result: 0
+        result: 0,
+        operationPerformed: ""
     };
     let divide = operation.operands[0];
     for (let i = 1; i < operation.operands.length; i++) {
         divide /= operation.operands[i];
     }
     operation.result = divide;
+    operation.operationPerformed = "/";
     history.push(operation);
     res.send(operation);
 });
 exports.router.post('/modulus', (req, res) => {
     const operation = {
         operands: req.body.operand,
-        result: 0
+        result: 0,
+        operationPerformed: ""
     };
     let mod = operation.operands[0];
     for (let i = 1; i < operation.operands.length; i++) {
         mod %= operation.operands[i];
     }
     operation.result = mod;
+    operation.operationPerformed = "%";
     history.push(operation);
     res.send(operation);
 });
